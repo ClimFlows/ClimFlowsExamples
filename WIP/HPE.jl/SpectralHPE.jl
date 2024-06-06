b95eccaa9fb89b1908656d92cb20873305b2dd5c
Thomas Dubos <thomas.dubos@lmd.ipsl.fr>
2024-06-06 21:48:03 +0200

    Non-allocating vertical remap

--- WIP/HPE.jl/SpectralHPE.jl
+++ WIP/HPE.jl/SpectralHPE.jl
@@ -3,13 +3,17 @@
 using Pkg; Pkg.activate(@__DIR__)
 using InteractiveUtils
 
+using ThreadPinning
+pinthreads(:cores)
+
 @time_imports begin
+    using MutatingOrNot: void
     using SIMDMathFunctions
     using LoopManagers: VectorizedCPU, MultiThread
 
     using CFTimeSchemes: CFTimeSchemes, advance!
     using CFDomains: SigmaCoordinate
-    using SHTnsSpheres: SHTnsSphere
+    using SHTnsSpheres: SHTnsSpheres, SHTnsSphere
 
     using ClimFluids: ClimFluids, IdealPerfectGas
     using CFPlanets: CFPlanets, ShallowTradPlanet
@@ -23,7 +27,9 @@ end
 # model setup
 
 function setup(choices, params, sph; hd_n = 8, hd_nu = 1e-2)
-    mgr = MultiThread(VectorizedCPU())
+#    mgr = MultiThread(VectorizedCPU())
+#    mgr = VectorizedCPU()
+    mgr = MultiThread()
     case = testcase(choices.TestCase, Float64)
     params = merge(choices, case.params, params)
     gas = params.Fluid(params)
@@ -52,18 +58,46 @@ end
 
 divisor(dt, T) = T / ceil(Int, T / dt)
 
-function vertical_remap(model, state)
-    reshp(x) = reshape(x, size(mass,1), size(mass,2), size(mass,3))
+function vertical_remap(model, state, scratch=void)
     sph = model.domain.layer
-    mass = SHTnsSpheres.synthesis_scalar!(void, state.mass_spec, sph)
-    uv = SHTnsSpheres.synthesis_vector!(void, state.uv_spec, sph)
-    now = @views (mass=mass[:,:,:,1], massq=mass[:,:,:,2], ux=uv.ucolat, uy=uv.ulon)
-    remapped = CFHydrostatics.vertical_remap!(nothing, model, void, void, now)
-    mass[:,:,:,1] .= reshp(remapped.mass)
-    mass[:,:,:,2] .= reshp(remapped.massq)
-    mass_spec = SHTnsSpheres.analysis_scalar!(void, mass, sph)
-    uv_spec = SHTnsSpheres.analysis_vector!(void, (ucolat=reshp(remapped.ux), ulon=reshp(remapped.uy)), sph)
+    mass_spat = SHTnsSpheres.synthesis_scalar!(scratch.mass_spat, state.mass_spec, sph)
+    uv_spat = SHTnsSpheres.synthesis_vector!(scratch.uv_spat, state.uv_spec, sph)
+    now = @views (mass=mass_spat[:,:,:,1], massq=mass_spat[:,:,:,2], ux=uv_spat.ucolat, uy=uv_spat.ulon)
+    remapped = CFHydrostatics.vertical_remap!(MultiThread(), model, scratch.remapped, scratch, now)
+
+    reshp(x) = reshape(x, size(mass_spat,1), size(mass_spat,2), size(mass_spat,3))
+    mass_spat[:,:,:,1] .= reshp(remapped.mass)
+    mass_spat[:,:,:,2] .= reshp(remapped.massq)
+    mass_spec = SHTnsSpheres.analysis_scalar!(state.mass_spec, mass_spat, sph)
+    uv_spec = SHTnsSpheres.analysis_vector!(state.uv_spec, (ucolat=reshp(remapped.ux), ulon=reshp(remapped.uy)), sph)
     return (; mass_spec, uv_spec)
+end
+
+function scratch_remap(diags, model, state)
+    flatten(x) = reshape(x, size(x,1)*size(x,2), size(x,3))
+    mass_spat = open(diags ; model, state).mass
+    uv_spat = open(diags ; model, state).uv
+    ux = flatten(similar(uv_spat.ulon))
+    uy = similar(ux)
+
+    mass = flatten(mass_spat[:,:,:,1])
+    new_mass, massq, slope, q = (similar(mass) for _ in 1:4)
+
+    flux = similar(mass, (size(mass,1), size(mass,2)+1))
+    fluxq = similar(flux)
+
+    return (; mass_spat, uv_spat, flux, fluxq, new_mass, slope, q, remapped=(; mass, massq, ux, uy))
+end
+
+function benchmark(model, state0, solver!, params, scratch=void ; ndays=1)
+    state = deepcopy(state0)
+    interval = params.interval
+    N = Int(ndays * 24 * 3600 / interval)
+    nstep = Int(params.interval / solver!.dt)
+    for iter = 1:N*nstep
+        advance!(state, solver!, state, 0.0, 1)
+        state = vertical_remap(model, state, scratch)
+    end
 end
 
 # main program
@@ -74,7 +108,7 @@ choices = (
     TestCase = Jablonowski06,
     Prec = Float64,
     nz = 30,
-    nlat = 48
+    nlat = 64
 )
 params = (
     ptop = 100,
@@ -84,13 +118,13 @@ params = (
     T0 = 300,
     radius = 6.4e6,
     Omega = 7.272e-5,
-    courant = 2,
+    courant = 1.8,
     interval = 6 * 3600, # 6-hour intervals
 )
 
 @info "Initializing spherical harmonics..."
 hasproperty(Main, :sph) || @time sph = SHTnsSphere(choices.nlat)
-
+@info sph
 @info "Model setup..."
 
 params = map(Float64, params)
@@ -101,18 +135,15 @@ solver! = solver(true) # mutating, non-allocating
 # solver = solver(false) # non-mutating, allocating
 
 @info "Time needed to simulate 1 day ..."
-new_state, t = let ndays = 1
-    interval = params.interval
-    N = Int(ndays * 24 * 3600 / interval)
-    nstep = Int(params.interval / solver!.dt)
-    state = deepcopy(state0)
-    advance!(state, solver!, state, 0.0, 1)
-    @time advance!(state, solver!, state, 0.0, N * nstep)
-end
+scratch = scratch_remap(diags, model, state0);
+benchmark(model, state0, solver!, params, scratch)
+@time benchmark(model, state0, solver!, params, scratch)
 
+# @profview benchmark(model, state0, solver!, params, scratch)
+#=
 @info "Preparing plots..."
 
-diag(state) = transpose(open(diags; model, state).uv.ucolat[:, :, 1])
+diag(state) = transpose(open(diags; model, state).temperature[:, :, 3])
 diag_obs = Makie.Observable(diag(state0))
 
 lons = Plots.bounds_lon(sph.lon[1, :] * (180 / pi)) #[1:2:end]
@@ -121,7 +152,6 @@ lats = Plots.bounds_lat(sph.lat[:, 1] * (180 / pi)) #[1:2:end]
 fig = Plots.orthographic(lons .- 90, lats, diag_obs; colormap = :berlin)
 
 @info "Starting simulation."
-
 @time let ndays = 10
     interval = params.interval
     N = Int(ndays * 24 * 3600 / interval)
@@ -139,16 +169,18 @@ fig = Plots.orthographic(lons .- 90, lats, diag_obs; colormap = :berlin)
                 #                toroidal = model.filter(toroidal, toroidal, model.sph)
                 #                uv_spec = vector_spec(uv_spec.spheroidal, toroidal)
                 #                state = RSW_state(gh_spec, uv_spec)
+                state = vertical_remap(model, state)
             end
-            state = vertical_remap(model, state)
             put!(ch, diag(state))
         end
         @info "Worker: finished"
     end
 
     # main thread making the movie
-    record(fig, "$(@__DIR__)/ulat.mp4", 1:N) do i
+    record(fig, "$(@__DIR__)/T850.mp4", 1:N) do i
         @info "t=$(div(interval*i,3600))h"
         diag_obs[] = take!(channel)
     end
 end
+
+=#
