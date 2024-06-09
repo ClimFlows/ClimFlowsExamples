6148c655443d6a3f53119bfdb13adf6833df4d59
Thomas Dubos <prenom.nom@lmd.ipsl.fr>
2024-06-10 00:42:11 +0200

    Pencils

--- WIP/Pencils/test_pencils.jl
+++ WIP/Pencils/test_pencils.jl
@@ -1,3 +1,6 @@
+# Usage:
+# srun -A wuu@cpu --ntasks=30 --hint=nomultithread julia test_pencils.jl
+
 using Pkg;
 Pkg.activate(@__DIR__);
 
@@ -7,10 +10,13 @@ using PencilArrays
 using PencilArrays.Transpositions: Transposition
 using SHTnsSpheres: SHTnsSphere, analysis_scalar!, void
 
-function setup()
+function setup(nlat, nz)
     MPI.Init()
     comm = MPI.COMM_WORLD
-    nlat, nz = 64, 30
+    is_master = MPI.Comm_rank(comm)==0
+    is_master && versioninfo()    
+
+    is_master && @info "Initializing MPI..."
     nlon = 2nlat
     horiz = nlon * nlat
     topo = MPITopology(comm, Val(1))
@@ -19,7 +25,12 @@ function setup()
     pencil1 = Pencil((horiz, nz), (2,), comm)
     pencil2 = Pencil(pencil1; decomp_dims=(1,))
 
-    sph = SHTnsSphere(nlat)
+    if is_master
+        @info "Initializing spherical harmonics..."
+        @time sph = SHTnsSphere(nlat)
+    else
+        sph = SHTnsSphere(nlat)
+    end
     return comm, pencil1, pencil2, sph
 end
 
@@ -27,25 +38,34 @@ function time(fun, N)
     fun()
     times = [(@timed fun()).time for _ in 1:N+10]
     sort!(times)
-    return round(sum(times[1:N])/N ; digits=6)
+    return round(sum(times[1:N])/N ; digits=7)
 end
 
-comm, p, q, sph = setup()
+function main()
+    nlat, nz = 128, 40
+    comm, p, q, sph = setup(nlat, nz)
 
-F=Float64
-a = Array{F}(undef, size_local(p))
-b = Array{F}(undef, size_local(q))
+    is_master = MPI.Comm_rank(comm)==0
 
-trans = Transposition(PencilArray(p, a), PencilArray(q, b))
+    F=Float64
+    a = Array{F}(undef, size_local(p))
+    b = Array{F}(undef, size_local(q))
 
-elapsed = time(10) do
-    transpose!(trans)
+    trans = Transposition(PencilArray(p, a), PencilArray(q, b))
+    
+    elapsed = time(10) do
+        transpose!(trans)
+    end
+    is_master && @info "Time for transposition: $elapsed"
+    
+    aa = reshape(a, nlat, 2nlat, :)
+    a_spec = analysis_scalar!(void, aa, sph)
+    is_master && @info "sizes" size(b) size(aa) size(a_spec)
+
+    elapsed = time(10) do
+        analysis_scalar!(a_spec, aa, sph)
+    end
+    is_master && @info "Time for spectral transform: $elapsed"
 end
 
-@info "Time for transposition: $elapsed"
-
-a_spec = analysis_scalar!(void, a, sph)
-elapsed = time(10) do
-    analysis_scalar!(a_spec, a, sph)
-end
-@info "Time for spectral transform: $elapsed"
+main()
