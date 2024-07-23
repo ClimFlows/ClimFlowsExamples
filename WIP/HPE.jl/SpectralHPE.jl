92a057d08a2e3a5f8d0d69520129b2e79cbcd667
Thomas Dubos <thomas.dubos@lmd.ipsl.fr>
2024-07-23 17:21:37 +0200

    Reorganize time loop

--- WIP/HPE.jl/SpectralHPE.jl
+++ WIP/HPE.jl/SpectralHPE.jl
@@ -10,9 +10,10 @@ pinthreads(:cores)
     using MutatingOrNot: void
     using SIMDMathFunctions
     using LoopManagers: PlainCPU, VectorizedCPU, MultiThread, tune
+    using CookBooks:CookBook
 
     using CFTimeSchemes: CFTimeSchemes, advance!
-    using CFDomains: SigmaCoordinate
+    using CFDomains: SigmaCoordinate, HyperDiffusion
     using SHTnsSpheres: SHTnsSpheres, SHTnsSphere, synthesis_scalar!
 
     using ClimFluids: ClimFluids, IdealPerfectGas
@@ -27,11 +28,86 @@ pinthreads(:cores)
 end
 
 Dynamics = Base.get_extension(CFHydrostatics, :SHTnsSpheres_Ext)
-# model setup
 
-function setup(choices, params, sph; hd_n = 8, hd_nu = 1e-2, mgr = MultiThread(VectorizedCPU(), nthreads))
-#    mgr = VectorizedCPU()
-#    mgr = MultiThread()
+#  custom time loop: combines HPE solver, time integration scheme, vertical remap and hyperdiffusion
+
+# everything that does not depend on initial condition
+struct TimeLoopInfo{Sphere, Dyn, Scheme, Filter}
+    sphere::Sphere
+    model::Dyn
+    scheme::Scheme
+    dissipation::Filter
+    diags::CookBook
+end
+
+# some info (essentially the time step) depends on the initial condition
+struct TimeLoop{Dyn, Solver, Filter}
+    model::Dyn
+    solver::Solver
+    dissipation::Filter
+    diags::CookBook
+    mutating::Bool
+end
+
+function max_time_step(info::TimeLoopInfo, courant, interval, state)
+    (; sphere, model, diags) = info
+    # time step based on maximum sound speed and courant number `courant`, which divides `interval`
+    session = open(diags; model, state)
+    uv = session.uv
+    cmax = maximum(session.sound_speed + @. sqrt(uv.ucolat^2 + uv.ulon^2))
+    dt = model.planet.radius * courant / cmax / sqrt(sphere.lmax * sphere.lmax + 1)
+    dt = divisor(dt, interval)
+end
+
+function TimeLoop(info::TimeLoopInfo, u0, time_step, mutating)
+    (; model, scheme, dissipation, diags) = info
+    solver = CFTimeSchemes.IVPSolver(scheme, time_step; u0, mutating)
+    return TimeLoop(model, solver, dissipation, diags, mutating)
+end
+
+# the extra parameter `dt` is for benchmarking purposes only
+function run(timeloop::TimeLoop, N, interval, state, scratch ; dt=timeloop.solver.dt)
+    (; solver, model, mutating) = timeloop
+    @assert mutating # FIXME
+    t, nstep = zero(dt), Int(interval / dt)
+    for _ = 1:N*nstep
+        advance!(state, solver, state, t, 1)
+        state = vertical_remap(model, state, scratch)
+    end
+end
+
+function new_setup(choices, params, sph; hd_n = 8, hd_nu = 1e-2, mgr = VectorizedCPU())
+    case = testcase(choices.TestCase, Float64)
+    params = merge(choices, case.params, params)
+    # stuff independent from initial condition
+    gas = params.Fluid(params)
+    vcoord = SigmaCoordinate(params.nz, params.ptop)
+    surface_geopotential(lon, lat) = initial_surface(lon, lat, case)[2]
+    model = HPE(params, mgr, sph, vcoord, surface_geopotential, gas)
+    scheme = CFTimeSchemes.RungeKutta4(model)
+    dissip = HyperDiffusion(model.domain, hd_n, hd_nu, :vector_curl)
+    diags = diagnostics(model)
+    info = TimeLoopInfo(sph, model, scheme, dissip, diags)
+
+    # initial condition
+    state = let
+        init(lon, lat) = initial_surface(lon, lat, case)
+        init(lon, lat, p) = initial_flow(lon, lat, p, case)
+        CFHydrostatics.initial_HPE(init, model)
+    end
+
+    return info, state
+end
+
+function new_benchmark(timeloop, state, dt, interval, scratch=void ; ndays=1)
+#    state = deepcopy(state0)
+    N = max(1, Int(ndays * 24 * 3600 / interval))
+    run(timeloop, N, interval, state, scratch ; dt)
+end
+
+# old model setup
+
+function setup(choices, params, sph; hd_n = 8, hd_nu = 1e-2, mgr = VectorizedCPU())
     case = testcase(choices.TestCase, Float64)
     params = merge(choices, case.params, params)
     gas = params.Fluid(params)
@@ -89,7 +165,7 @@ end
 function benchmark(model, state0, solver!, params, scratch=void ; ndays=1)
     state = deepcopy(state0)
     interval = params.interval
-    N = Int(ndays * 24 * 3600 / interval)
+    N = max(1, Int(ndays * 24 * 3600 / interval))
     nstep = Int(params.interval / solver!.dt)
     for iter = 1:N*nstep
         advance!(state, solver!, state, 0.0, 1)
@@ -105,7 +181,7 @@ choices = (
     TestCase = Jablonowski06,
     Prec = Float64,
     nz = 30,
-    nlat = 128
+    nlat = 64
 )
 params = (
     ptop = 100,
@@ -121,7 +197,7 @@ params = (
 
 @info "Initializing spherical harmonics..."
 nthreads = max(1,Threads.nthreads()-1)
-hasproperty(Main, :sph) || @time sph = SHTnsSphere(choices.nlat, nthreads)
+(hasproperty(Main, :sph) && sph.nlat==choices.nlat) || @time sph = SHTnsSphere(choices.nlat, nthreads)
 @info sph
 
 @info "Model setup..."
@@ -129,22 +205,27 @@ hasproperty(Main, :sph) || @time sph = SHTnsSphere(choices.nlat, nthreads)
 params = map(Float64, params)
 params = (Uplanet = params.radius * params.Omega, params...)
 
-@time model, state0, diags, scheme, solver = setup(choices, params, sph)
+cpu, simd = PlainCPU(), VectorizedCPU(8)
+mgrs = [cpu, simd, MultiThread(cpu, nthreads), MultiThread(simd, nthreads)]
 
-vsize, cpu, simd = 16, PlainCPU(), VectorizedCPU()
-mgrs = [simd, MultiThread(cpu, nt), MultiThread(simd, nt)]
 for mgr in mgrs
+    # NB : spherical harmonics are multithread in all cases !
     @info "===== Time needed to simulate 1 day with $mgr ====="
-    model, state0, diags, scheme, solver = setup(choices, params, sph ; mgr)
-    scratch = scratch_remap(diags, model, state0);
-    solver! = solver(true) # mutating, non-allocating
-    benchmark(model, state0, solver!, params, scratch)
-    @time benchmark(model, state0, solver!, params, scratch)
-    # @profview benchmark(model, state0, solver!, params, scratch)
+    let (info, state0) = new_setup(choices, params, sph; mgr)
+        scratch = scratch_remap(info.diags, info.model, state0)
+        (; courant, interval) = params
+        dt = max_time_step(info, courant, interval, state0)
+        timeloop = TimeLoop(info, state0, 0.0, true) # zero time step for benchmarking
+        new_benchmark(timeloop, state0, dt, interval ; ndays=0) # compile
+        @time new_benchmark(timeloop, state0, dt, interval, scratch)
+#        @profview new_benchmark(timeloop, state0, dt, interval, scratch)
+    end
 end
 
-solver! = solver(true) # mutating, non-allocating
-# solver = solver(false) # non-mutating, allocating
+# model, state0, diags, scheme, solver = setup(choices, params, sph)
+
+info, state0 = new_setup(choices, params, sph; mgr=simd)
+(; diags, model) = info
 
 @info "Preparing plots..."
 
@@ -158,24 +239,25 @@ fig = Plots.orthographic(lons .- 90, lats, diag_obs; colormap = :berlin)
 
 @info "Starting simulation."
 @time let ndays = 10
-    interval = params.interval
+    scratch = scratch_remap(diags, model, state0);
+    (; courant, interval) = params
+    dt = max_time_step(info, courant, interval, state0)
+    timeloop = TimeLoop(info, state0, dt, true) # mutating, non-allocating
     N = Int(ndays * 24 * 3600 / interval)
 
     # separate thread running the simulation
     channel = Channel(spawn = true) do ch
         state = deepcopy(state0)
-        nstep = Int(params.interval / solver!.dt)
+#        nstep = Int(params.interval / solver!.dt)
         for iter = 1:N
+            #=
             for istep = 1:nstep
                 advance!(state, solver!, state, 0.0, 1)
-                #                state, _ = advance!(void, solver, state, 0.0, 1)
-                #                (; gh_spec, uv_spec) = state
-                #                (; toroidal) = uv_spec
-                #                toroidal = model.filter(toroidal, toroidal, model.sph)
-                #                uv_spec = vector_spec(uv_spec.spheroidal, toroidal)
-                #                state = RSW_state(gh_spec, uv_spec)
                 state = vertical_remap(model, state, scratch)
             end
+            =#
+            run(timeloop, 1, interval, state, scratch)
+
             put!(ch, diag(state))
         end
         @info "Worker: finished"
