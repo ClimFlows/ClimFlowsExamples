20cf307ae67e8360d13290f6c53644e7940fdfae
Thomas Dubos <thomas.dubos@polytechnique.edu>
2025-08-01 23:50:04 +0200

    NH dynamics done, except HEVI solver

--- WIP/Compressible.jl/Compressible.jl
+++ WIP/Compressible.jl/Compressible.jl
@@ -4,11 +4,20 @@ using Pkg; Pkg.activate(@__DIR__);
 push!(LOAD_PATH, Base.Filesystem.joinpath(@__DIR__, "packages")); unique!(LOAD_PATH)
 using Revise
 
-using CFCompressible
+@time_imports using CFCompressible
 
 includet("setup.jl");
 include("config.jl");
 includet("run.jl");
+
+CFTimeSchemes.tendencies!(slow, fast, scratch, model::CFCompressible.FCE, state, t, dt) = 
+    CFCompressible.tendencies!(slow, fast, scratch, model, state, t, dt )
+
+function CFTimeSchemes.model_dstate(::CFCompressible.FCE, state, _)
+    rsim(x::AbstractArray) = similar(x)
+    rsim(x::NamedTuple) = map(rsim, x)
+    return rsim(state)
+end
 
 #============================  main program =========================#
 
@@ -17,7 +26,7 @@ nthreads = 1 # Threads.nthreads()
 cpu, simd = PlainCPU(), VectorizedCPU(8)
 mgr = (nthreads>1) ? MultiThread(simd, nthreads) : simd
 
-mgr = cpu
+# mgr = cpu
 
 @info "Initializing spherical harmonics..."
 (hasproperty(Main, :sph) && sph.nlat == choices.nlat) ||
@@ -48,11 +57,20 @@ loop_HPE, case = setup(choices, params, sph, mgr, HPE)
 state_HPE =  CFHydrostatics.initial_HPE(case, model)
 @profview tape = simulation(merge(choices, params), loop_HPE, deepcopy(state_HPE));
 
-compressible = CFCompressible.FCE(model, params.gravity)
-state_FCE = CFCompressible.NH_state.diagnose(compressible, diags, state_HPE)
-# loop_FCE, case = setup(choices, params, sph, mgr, CFCompressible.FCE)
+ps = open(diags; model, state=state_HPE).surface_pressure
+model_FCE = CFCompressible.FCE(model, params.gravity, ps, params.rhob)
+state_FCE = CFCompressible.NH_state.diagnose(model_FCE, diags, state_HPE)
+scheme_FCE = choices.TimeScheme(model_FCE)
 
-# @time tape = simulation(merge(choices, params), loop_HPE, state0);
+diags_FCE = CFCompressible.diagnostics(model_FCE)
+let session = open(diags_FCE ; model=model_FCE, state=state_FCE)
+    @info "check" extrema(session.temperature)
+    @info "check" extrema(session.conservative_variable)
+    @info "check" extrema(session.pressure)
+end
+
+loop_FCE = TimeLoopInfo(sph, model_FCE, scheme_FCE, loop_HPE.remap_period, loop_HPE.dissipation, diags_FCE)
+@time tape = simulation(merge(choices, params), loop_FCE, state_FCE);
 
 #=
 include("movie.jl")
--- WIP/Compressible.jl/packages/CFCompressible/src/CFCompressible.jl
+++ WIP/Compressible.jl/packages/CFCompressible/src/CFCompressible.jl
@@ -3,21 +3,27 @@ module CFCompressible
 using CFHydrostatics: HPE
 using CFPlanets: ShallowTradPlanet
 
-struct FCE{F, Manager, Coord, Domain, Fluid, TwoDimScalar}
+struct SoftSurface{T, A<:AbstractMatrix{T}} 
+end
+
+struct FCE{F, Manager, Coord, Domain, Fluid, TwoDimScalar<:AbstractMatrix{F}}
     mgr::Manager
     vcoord::Coord
     planet::ShallowTradPlanet{F}
     domain::Domain
     gas::Fluid
     fcov::TwoDimScalar # covariant Coriolis factor = f(lat)*radius^2
-    Phis::TwoDimScalar # surface geopotential
+    # bottom boundary condition: p = pb - rhob*(Phi-Phis)
+    Phis::TwoDimScalar # target surface geopotential
+    pb::TwoDimScalar # surface pressure when Phi == Phis
+    rhob::F # larger rhob makes smaller Phi-Phis but stiffer system
 end
 
-function FCE(model::HPE{F}, gravity::F) where F
+function FCE(model::HPE{F}, gravity::F, pb, rhob) where F
     (; mgr, vcoord, planet, domain, gas, fcov, Phis) = model
-    (; Omega, radius) = planet
-    planet = ShallowTradPlanet(Omega, gravity, radius)
-    return FCE(mgr, vcoord, planet, domain, gas, fcov, Phis)
+    (; radius, Omega) = planet
+    planet = ShallowTradPlanet(radius, Omega, gravity)
+    return FCE(mgr, vcoord, planet, domain, gas, fcov, Phis, pb, rhob)
 end
 
 # implemented in Dynamics
@@ -36,6 +42,9 @@ include("vertical_dynamics.jl")
 include("horizontal_energies.jl")
 include("dynamics.jl")
 include("NH_state.jl")
+include("diagnostics.jl")
 include("tests.jl")
+
+diagnostics(::FCE) = Diagnostics.diagnostics()
 
 end # module CFCompressible
