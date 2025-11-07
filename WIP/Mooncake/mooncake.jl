c9fa1517d3950d31e90f88809b3df199fc19d6c6
Thomas Dubos <thomas.dubos@polytechnique.edu>
2025-11-07 17:09:51 +0100

    Moved Voronoi operators and Mooncake adjoints to CFDomains 0.3.10

--- WIP/Mooncake/mooncake.jl
+++ WIP/Mooncake/mooncake.jl
@@ -1,4 +1,4 @@
-using Pkg; Pkg.activate(@__DIR__)
+using Pkg; Pkg.activate(@__DIR__); Pkg.precompile()
 using Revise
 using InteractiveUtils
 using BenchmarkTools
@@ -8,49 +8,20 @@ using Cthulhu
     import DifferentiationInterface as DI
     using DifferentiationInterface: Constant as Const
     import Mooncake
-
+    import Enzyme
+        
     using FixedSizeArrays
+    using LinearAlgebra: dot, norm
 
     using ClimFlowsData: DYNAMICO_meshfile, DYNAMICO_reader
     using NetCDF: ncread
     using CFDomains: VoronoiSphere, Stencils
+    import CFDomains.VoronoiOperators as Ops
 end
-
-includet("stubs.jl")
-includet("fixed_size.jl")
-includet("rrules.jl")
-
-include("check_fixed_size.jl")
 
 #=============================================================================#
 
 includet("stencils.jl")
-
-function sum_op2(f, tmp, op, app!)
-    app!(tmp, op, f)
-    return sum(x->x^2, tmp)
-end
-
-function test_op(q, tmp, op)
-    @info "forward" sum_op2(q, tmp, op, apply!_internal)
-    @code_warntype sum_op2(q, tmp, op, apply!_internal)
-    
-    backend = DI.AutoMooncake(; config=nothing)
-    # prep_internal = DI.prepare_gradient(sum_op2, backend, q, Const(tmp), Const(op), Const(apply!_internal));
-    # grad_internal = DI.gradient(sum_op2, prep_internal, backend, q, Const(tmp), Const(op), Const(apply!_internal));
-    prep = DI.prepare_gradient(sum_op2, backend, q, Const(tmp), Const(op), Const(apply!));
-    grad = DI.gradient(sum_op2, prep, backend, q, Const(tmp), Const(op), Const(apply!));
-    # @assert grad ≈ grad_internal    
-
-    # run_internal() = DI.gradient(sum_op2, prep_internal, backend, q, Const(tmp), Const(op), Const(apply!_internal))
-    run() = DI.gradient(sum_op2, prep, backend, q, Const(tmp), Const(op), Const(apply!))
-    for _ in 1:10
-        @time run()
-        # @time run_internal()
-    end
-    display(@benchmark $run())
-    # display(@benchmark $run_internal())
-end
 
 # `choices` is for discrete parameters, while `params` is for continuous parameters (floats)
 # Values in `params` will be converted to `choices.precision`.
@@ -63,15 +34,24 @@ choices = (
 reader = DYNAMICO_reader(ncread, choices.meshname)
 vsphere = VoronoiSphere(reader; prec=choices.precision)
 
-#=============== TRiSK ================#
-
-q = randn(choices.precision, length(vsphere.lon_e))
-tmp = similar(q)
-test_op(q, tmp, TRiSK(vsphere))
-
 #=============== Gradient ================#
 
 q = randn(choices.precision, length(vsphere.lon_i))
 tmp = similar(q, length(vsphere.lon_e)) # gradient is computed on edges
-grad_op = Gradient(vsphere)
-test_op(q, tmp, grad_op)
+test_op(q, tmp, Ops.Gradient(vsphere))
+
+exit()
+
+#=============== TRiSK ================#
+
+q = randn(choices.precision, length(vsphere.lon_e))
+tmp = similar(q)
+test_op(q, tmp, Ops.TRiSK(vsphere))
+
+#=============================================================================#
+
+includet("stubs.jl")
+includet("fixed_size.jl")
+includet("rrules.jl")
+
+include("check_fixed_size.jl")
--- WIP/Mooncake/stencils.jl
+++ WIP/Mooncake/stencils.jl
@@ -1,109 +1,34 @@
-using ManagedLoops: @unroll
-
-# each operator holds the data needed to compute itself and also to compute its adjoint
-abstract type DiffStencil{In,Out} end
-
-ignore(x,y) = y # default action
-
-struct Gradient{Action} <: DiffStencil{1,1}
-    action::Action # how to combine op(input) with output
-    edge_left_right::Matrix{Int32}
-    # for the adjoint
-    primal_deg::Vector{Int32}
-    primal_edge::Matrix{Int32}
-    primal_ne::Matrix{Int32}
+function LRSW!(dm, ducov, vsphere, m, ucov)
+    Ops.Divergence(vsphere, minus)(dm, ucov)  # ∂m/∂t = -∇⋅u
+    Ops.Gradient(vsphere, minus)(ducov, m)    # ∂u/∂t = -∇m
+    Ops.TRiSK(vsphere, subfrom)(ducov, ucov)  # ∂u/∂t -= u⟂
 end
-Gradient(sph) = Gradient(ignore, sph)
-Gradient(action, sph) = Gradient(action, sph.edge_left_right, sph.primal_deg, sph.primal_edge, sph.primal_ne)
 
-# divergence operator, without dividing by Ai : result is a 2-form
-struct DivForm{Action} <: DiffStencil{1,1}
-    action::Action # how to combine op(input) with output
-    primal_deg::Vector{Int32}
-    primal_edge::Matrix{Int32}
-    primal_ne::Matrix{Int32}
-    # for the adjoint
-    edge_left_right::Matrix{Int32}
+function sum_op2(f, tmp, op, app!)
+    app!(tmp, op, f)
+    return norm(tmp)
 end
-DivForm(action, sph) = DivForm(action, sph.primal_deg, sph.primal_edge, sph.primal_ne, sph.edge_left_right)
 
-struct TRiSK{Action, F} <: DiffStencil{1,1}
-    action::Action # how to combine op(input) with output
-    trisk_deg::Vector{Int32}
-    trisk::Matrix{Int32}
-    wee::Matrix{F}
-end
-TRiSK(sph) = TRiSK(ignore, sph)
-TRiSK(action, sph) = TRiSK(action, sph.trisk_deg, sph.trisk, sph.wee)
+function test_op(q, tmp, op)
+    @info "forward" sum_op2(q, tmp, op, Ops.apply_internal!)
+    @code_warntype sum_op2(q, tmp, op, Ops.apply_internal!)
+    
+    backend = DI.AutoMooncake(; config=nothing)
+    prep = DI.prepare_gradient(sum_op2, backend, q, Const(tmp), Const(op), Const(Ops.apply!));
+    grad = DI.gradient(sum_op2, prep, backend, q, Const(tmp), Const(op), Const(Ops.apply!));
+    prep_internal = DI.prepare_gradient(sum_op2, backend, q, Const(tmp), Const(op), Const(Ops.apply_internal!));
+    grad_internal = DI.gradient(sum_op2, prep_internal, backend, q, Const(tmp), Const(op), Const(Ops.apply_internal!));
+    @assert grad ≈ grad_internal    
 
-apply!(output, stencil::DiffStencil{1,1}, input) = apply!_internal(output, stencil, input)
-
-@inline function apply!_internal(output, op::Gradient, input)
-    (; action) = op
-    @inbounds for edge in eachindex(output)
-        grad = Stencils.gradient(op, edge)
-        output[edge] = action(output[edge], grad(input))
+    let
+        run() = sum_op2(q, tmp,op, Ops.apply!)
+        display(@benchmark $run())
     end
-    return nothing
-end
-
-@inline function apply!_internal(output, op::DivForm, input)
-    (; action) = op
-    @inbounds for cell in eachindex(output)
-        deg = op.primal_deg[cell]
-        @unroll deg in 5:7 begin
-            dvg = Stencils.div_form(op, cell, Val(deg))
-            output[cell] = action(output[cell], dvg(input))
+    let
+        run() = DI.gradient(sum_op2, prep, backend, q, Const(tmp), Const(op), Const(Ops.apply!))
+        display(@benchmark $run())
+        @profview for _ in 1:10
+            DI.gradient(sum_op2, prep, backend, q, Const(tmp), Const(op), Const(Ops.apply!))
         end
     end
-    return nothing
-end
-
-@inline function apply!_internal(output, op::TRiSK, input)
-    (; action) = op
-    for edge in eachindex(output)
-        deg = op.trisk_deg[edge]
-        @unroll deg in 9:11 begin
-            trsk = Stencils.TRiSK(op, edge, Val(deg))
-            output[edge] = action(output[edge], trsk(input))
-        end
-    end
-    return nothing
-end
-
-module StencilRules
-
-# codual = reverse codual = primal + rdata
-# fcodual = forward codual = primal + fdata
-
-using Mooncake: Mooncake, CoDual, NoTangent, NoFData, NoRData, zero_fcodual, primal
-using Main: apply!, apply!_internal, ignore, DiffStencil, Gradient, DivForm, TRiSK
-
-Mooncake.tangent_type(::Type{<:DiffStencil}) = NoTangent
-
-const CoVector{F} = CoDual{<:AbstractVector{F}, <:AbstractVector{F}}
-const CoNumber{F} = CoDual{F,NoFData}
-const CoStencil{A,B} = CoDual{<:DiffStencil{A,B}, NoFData}
-CoFunction(f) = CoDual{typeof(f), NoFData}
-
-Mooncake.@is_primitive Mooncake.DefaultCtx Tuple{typeof(apply!), Vararg}
-Mooncake.rrule!!(::CoFunction(apply!), fx::Vararg) = apply!_rrule!!(fx...)
-
-function apply!_rrule!!(foutput::CoVector{F}, op::CoStencil{1,1}, finput::CoVector{F}) where F
-    output, stencil, input = primal(foutput), primal(op), primal(finput)
-    output_ = copy(output) # to undo mutation during backward pass    
-    dout, din, adj = tangent(foutput), tangent(finput), adjoint(stencil)
-    function apply!_pullback!!(::NoRData)
-        @. output = output_ # undo mutation
-        apply!_internal(din, adj, dout)
-        return NoRData(), NoRData(), NoRData(), NoRData() # rdata for (apply!, output, op, input)
-    end
-    apply!_internal(output, stencil, input)
-    return zero_fcodual(nothing), apply!_pullback!!
-end
-
-adjoint(op::Gradient{typeof(ignore)}) = DivForm(subfrom, op)
-adjoint(op::TRiSK{typeof(ignore)}) = TRiSK(subfrom, op)
-subfrom(x,y) = x-y
-
 end
